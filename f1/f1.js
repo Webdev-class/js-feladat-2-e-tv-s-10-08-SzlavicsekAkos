028268108381e61a26de85b7ce8a2b87d927dfa6
Pokicaelnok <hunor.pok@gmail.com>
2024-10-08 13:04:43 +0200

    teszt átment

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,13 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const querryString = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`
+    const arr = await fetch(querryString).then(data => data.json())
+    let sum = 0;
+    arr.hourly.temperature_2m.forEach(element => {
+        sum += element;
+    });
+    return Math.floor(sum/24);
 }
-
+f1(52.52, 13.41, "2024-10-04")
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,9 @@
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    const querryString = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`
+    const arr = await fetch(querryString).then(data => data.json())
+    return arr.hourly.temperature_2m.at(hour)
+
 }
 module.exports = f2;
